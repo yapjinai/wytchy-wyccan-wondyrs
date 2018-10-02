d7cb95f11e807719db267239cacb75b16f094c59
yapjinai <jinai.jyap@gmail.com>
2018-10-02 17:49:00 -0400

    changed user migration

--- db/migrate/20181001154026_create_users.rb
+++ db/migrate/20181001154026_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :username
+      t.text :password_digest
+
       t.string :gender
       t.datetime :birthday
       t.string :bloodtype
