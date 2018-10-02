54f453f7074f964d9ff352e179e0ac783a05b04e
Chris Bojemski <chris.bojemski@gmail.com>
2018-10-02 13:36:49 -0400

    Fix profile edit page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-Spell.delete_all
-Item.delete_all
-
 s1 = Spell.create(
   name: "Death",
   description: "Causes someone to die a slow, painful death. Use with discretion"
@@ -24,6 +21,12 @@ i1 = Item.create(
 i2 = Item.create(
   name: "Twig"
 )
+i3 = Item.create(
+  name: 'Crushed Clove'
+)
+Item.create(name: 'Squirrel Skull')
+Item.create(name: 'Spider Ichor')
 
-s1.items << i1
-s2.items << i2
+ItemsSpell.create(spell: s1, item: i1, quantity: 4)
+ItemsSpell.create(spell: s1, item: i2, quantity: 2)
+ItemsSpell.create(spell: s1, item: Item.last, quantity: 1)
