45737bec1c0596f5c3d57db64bcf940ee6e4c9ea
Chris Bojemski <chris.bojemski@gmail.com>
2018-10-03 14:16:39 -0400

    Add item and spell parser to seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,74 @@
-Spell.delete_all
-Item.delete_all
-Recipe.delete_all
+def create_recipe(spell_hash)
+  symbols = spell_hash[:recipe].keys
+  symbols.each do |s|
+    spell = Spell.find_by(name: spell_hash[:title])
+    item = Item.find_by(symbol: s)
+    quantity = spell_hash[:recipe][s]
+    r = Recipe.create(spell: spell, item: item, quantity: quantity)
+    # byebug
+  end
+end
 
-s1 = Spell.create(
-  name: "Death",
-  description: "Causes someone to die a slow, painful death. Use with discretion"
-)
-s2 = Spell.create(
-  name: "Love",
-  description: "Causes people to fall in love"
-)
-s3 = Spell.create(
-  name: 'Broken Arrow',
-  description: 'Cease conflict between you and an acquaintance.'
-)
-s4 = Spell.create(
-  name: 'Hex',
-  description: 'Damn someone to unluckiness for 3 days.'
-)
+def create_spell(spell)
+  Spell.create(
+    name: spell[:title],
+    description: spell[:description]
+  )
+end
 
-i1 = Item.create(name: "Mushroom")
-i2 = Item.create(name: "Twig")
-i3 = Item.create(name: 'Crushed Clove')
-i4 = Item.create(name: 'Squirrel Skull')
-i5 = Item.create(name: 'Spider Ichor')
+def parse_recipe(spell)
+  # byebug
+  recipe_string = spell[:recipe_string]
+  spell[:recipe] = {}
+  recipe = recipe_string.split(',')
+  recipe.each do |r|
+    symbol = r.split('x')[0].strip
+    quantity = r.split('x')[1].to_i
+    # byebug
+    spell[:recipe][symbol] = quantity
+  end
+  spell
+end
 
-Recipe.create(spell: s1, item: i1, quantity: 4)
-Recipe.create(spell: s1, item: i2, quantity: 2)
-Recipe.create(spell: s1, item: i3, quantity: 1)
-Recipe.create(spell: s2, item: i5, quantity: 3)
-Recipe.create(spell: s2, item: i4, quantity: 2)
-Recipe.create(spell: s3, item: i3, quantity: 1)
-Recipe.create(spell: s3, item: i1, quantity: 3)
-Recipe.create(spell: s4, item: i2, quantity: 1)
-Recipe.create(spell: s4, item: i3, quantity: 2)
-Recipe.create(spell: s4, item: i4, quantity: 5)
-Recipe.create(spell: s4, item: i5, quantity: 1)
+def parse_spell(line)
+  spell = {}
+  spell_info = line.split('=')[0].strip
+  spell[:recipe_string] = line.split('=')[1].strip
+  spell[:title] = spell_info.split(';')[0].strip.titleize
+  spell[:description] = spell_info.split(';')[1].strip
+  spell
+end
+
+def parse_item(line)
+  item = {}
+  # puts line
+  item[:emoji] = line.split(':')[0].strip
+  item[:title] = line.split(':')[1].strip.titleize
+  item
+end
+
+def create_item(item)
+  Item.create(name: item[:title], symbol: item[:emoji])
+end
+
+spells_delim = false
+puts "Making items..."
+File.open('db/emoji-map.txt').each do |line|
+  # once the emojis are finished, we delin with a blank line
+
+  if line == "\n"
+    spells_delim = true
+    puts "Making spells and recipes..."
+    next
+  end
+
+  if !spells_delim
+    item = parse_item(line)
+    create_item(item)
+  else
+    spell = parse_spell(line)
+    spell = parse_recipe(spell)
+    create_spell(spell)
+    create_recipe(spell)
+  end
+end
