f80731cb462d8cc31f64d76c65c07f1c95471103
Chris Bojemski <chris.bojemski@gmail.com>
2018-10-05 09:57:46 -0400

    Add duration to spell show page.

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -4,4 +4,22 @@ class Spell < ApplicationRecord
 
   has_many :castings
   has_many :users, through: :castings #who cast them
+
+  def duration_props
+    props = {}
+    if self.duration > 3600
+      duration = self.duration / 3600
+      label = 'hours'
+    elsif self.duration > 240
+      duration = self.duration / 60
+      label = 'minutes'
+    else
+      duration = self.duration
+      label = 'seconds'
+    end
+    props[:duration] = duration
+    props[:label] = label
+    byebug
+    props
+  end
 end
